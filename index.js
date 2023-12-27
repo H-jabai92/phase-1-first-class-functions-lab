bb53a03ff0b0bd1d1bc048a7dd1f8142e843ef6a
Hasan <h.mjabai92@gmail.com>
2023-12-27 10:46:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const func = function () {
+  console.log("wlc");
+};
+
+function returnFirstTwoDrivers(e) {
+  return e[0] + e[1];
+}
+returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]);
+// => ['Antonia', 'Nuru']~
+
+function returnFirstTwoDrivers(e) {
+  return e[2] + e[3];
+}
+returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]);
+// => ['Amari', 'Mo']~
